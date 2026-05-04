c2b9878a9cd01b3076688e069e3d746be9221f6d
Rati Krishna Moharana <mratikrishna@gmail.com>
2026-05-04 13:59:01 +0530

    project completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DocumentsComponent } from './components/documents/documents.component';
+import { ChatComponent } from './components/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'documents', component: DocumentsComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: '**', redirectTo: '' }
+];
